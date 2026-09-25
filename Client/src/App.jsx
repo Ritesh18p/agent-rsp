fe769ccce8a62719c2de3194c265cd4c49d1bc33
Ritesh Purud <riteshpurud7@gmail.com>
2026-09-25 16:38:47 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import Home from "./pages/home";
-import Login from "./pages/login";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Register from "./pages/register";
 import Dashboard from "./pages/dashboard";
 
